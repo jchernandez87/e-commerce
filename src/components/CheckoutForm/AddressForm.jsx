31b065f007b078d050f65960c3e510b5c1e41171
Juan Carlos <jchernandez827@gmail.com>
2022-09-27 11:08:05 -0500

    setup react hook form, and custom text field

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,5 +1,31 @@
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useForm, FormProvider } from "react-hook-form";
+import FormInput from "./CustomTextField";
+
 const AddressForm = () => {
-  return <h1>Address form</h1>;
+  const methods = useForm();
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit="">
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="first name" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,18 @@
+import { TextField, Grid } from "@mui/material";
+import { useFormContext, Controller } from "react-hook-form";
+
+const FormInput = ({ name, label, required }) => {
+  const { control } = useFormContext();
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        control={control}
+        name={name}
+        render={() => <TextField fullWidth required={required} label={label} />}
+      />
+    </Grid>
+  );
+};
+
+export default FormInput;
