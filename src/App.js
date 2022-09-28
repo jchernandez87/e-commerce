68fb802dcf27f56c6af6f15292d10bc19212c606
Juan Carlos <jchernandez827@gmail.com>
2022-09-28 15:43:33 -0500

    setup stripe with commercejs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { commerce } from "./lib/commerce";
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -38,6 +40,21 @@ const App = () => {
     await commerce.cart.empty().then((updatedCart) => setCart(updatedCart));
   };
 
+  const refreshCart = async () =>  {
+    const newCart = await commerce.cart.refresh()
+    setCart(newCart)
+  }
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+      setOrder(incomingOrder)
+      refreshCart()
+    } catch(error) {
+        setErrorMessage(error.data.error.message)
+    }
+  }
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -65,7 +82,7 @@ const App = () => {
             />
           }
         />
-        <Route path="/checkout" element={<Checkout cart={cart} />} />
+        <Route path="/checkout" element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage}/>} />
       </Routes>
     </div>
   );
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -16,7 +16,7 @@ import styles from "./Styles";
 
 const steps = ["Shipping address", "Payment details"];
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [shippingData, setShippingData] = useState({});
@@ -43,15 +43,13 @@ const Checkout = ({ cart }) => {
     nextStep();
   };
 
-  console.log(shippingData)
-
   const Confirmation = () => <h1>Confirmation</h1>;
 
   const Form = () =>
     activeStep === 0 ? (
       <AddressForm next={next} checkoutToken={checkoutToken} />
     ) : (
-      <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
+      <PaymentForm nextStep={nextStep} backStep={backStep} shippingData={shippingData} checkoutToken={checkoutToken} onCaptureCheckout={onCaptureCheckout} />
     );
 
   return (
